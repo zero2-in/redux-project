a342346b6468df0d6eca66bc5d6abf30795bcdbe
zero2-in <wowin21@gmail.com>
2022-03-31 15:20:25 +0900

    Redux: Store
    Store는 어플리케이션의 현재상태를 지니고 있음.
    dispatch(action) = 액션을 리듀서로 보냄
    getState() = 현재 상태를 반환
    subscribe(listener) = 상태가 바뀔때마다 실행할 함수 등록
    listener는 콜백 함수

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from "react-dom/client";
 
 import App from "./components/App";
 
+import { createStore } from "redux";
+import reducers from './reducers';
+
+const store = createStore(reducers);
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App/>);
